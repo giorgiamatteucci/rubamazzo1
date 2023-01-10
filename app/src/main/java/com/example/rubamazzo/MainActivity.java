32bba7b1352791c3729a5b841423105fc322e87c
enricolucarelliSIRCase <enrico.lucarelli4@studio.unibo.it>
2023-01-10 22:10:03 +0100

    prova 01

--- app/src/main/java/com/example/rubamazzo/MainActivity.java
+++ app/src/main/java/com/example/rubamazzo/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.rubamazzo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvTitolo;
+    Button btnRegistrati, btnAccedi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //commento per prova commit
+        //binding elementi layout con elementi java (oggetti)
+        tvTitolo = findViewById(R.id.tvTiitolo);
+        btnRegistrati = findViewById(R.id.btnRegistrati);
+        btnAccedi = findViewById(R.id.btnAccedi);
+
+        btnAccedi.setOnClickListener(v -> {
+            tvTitolo.setText(" hai scelto accedi ");
+        });
+
     }
 }
